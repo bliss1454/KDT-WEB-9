e41bb0b2c71396a34723b7cb50232195b1c47521
bliss1454 <rkgus1454@naver.com>
2023-07-28 17:12:31 +0900

    0728 node.js

---
+++ Node.js/07_nodejs_2/실습2.js
@@ -0,0 +1,16 @@
+const express = require ('express');
+const app = express();
+const PORT = 8000;
+
+app.set('view engine', 'ejs');
+app.set('views','./views');
+
+app.use('/public_',express.static('./public_'))
+
+app.get('/',(req, res) => {
+    res.render('실습2');
+});
+
+app.listen(PORT, () => {
+    console.log(`http://localhost:${PORT}`)
+})
