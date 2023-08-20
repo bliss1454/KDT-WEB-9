ff41b920539ec491f3bdcf8a82d3f400f450d407
bliss <rkgus1454@naver.com>
2023-08-20 20:18:39 +0900

    jwt

--- Node.js/19_jwt_practice/index.js
+++ Node.js/19_jwt_practice/index.js
@@ -1,8 +1,8 @@
 const express = require('express');
-const jwt = require('jsonwebtoken');
+const jwt = require('jsonwebtoken');  //jwt를 불러옴
 const app = express();
 const PORT = 8000;
-const SECRET = 'y5c1n79bQ9LxMoiaIn4vb9u2WkomhviT';
+const SECRET = 'y5c1n79bQ9LxMoiaIn4vb9u2WkomhviT'; //랜덤으로 생성해주는 키를 가져온 것
 
 const userInfo = { id: 'kdt9', pw: '1234', name: '홍길동' };
 
@@ -18,7 +18,8 @@ app.get('/login', (req, res) => {
 });
 app.get('/token', (req, res) => {
     if (req.headers.authorization) {
-        const token = req.headers.authorization.split(' ');
+        //bearer token값--> token[0]에 들어감 그 후의 사용자의 토큰값은 token[1]로 들어옴. 이 밖의 다른 사용자의 경우는, 아이디값이 다르기에 똑같이 token[1]임.
+        const token = req.headers.authorization.split(' '); //split은 문자열을 끊어주는((' '):띄어쓰기를 기준으로 끊어라) 역할을 함.
         try {
             const result = jwt.verify(token[1], SECRET);
             if (result.id === userInfo.id) {
@@ -37,7 +38,7 @@ app.post('/login', (req, res) => {
         const { id, pw } = req.body;
         const { id: uId, pw: uPw } = userInfo;
         if (id === uId && pw === uPw) {
-            const token = jwt.sign({ id }, SECRET);
+            const token = jwt.sign({ id }, SECRET); //토큰이랑 같이 보내고싶은 값을 작성해줌
             res.json({ result: true, token });
         } else {
             res.status(404).json({ result: false, message: '로그인 정보가 올바르지 않습니다' });
