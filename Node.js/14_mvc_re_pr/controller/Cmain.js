427a8e09f1e7cd79030c20547d537b11aec00a64
bliss <rkgus1454@naver.com>
2023-08-23 22:31:04 +0900

    mvc_mysql

--- Node.js/14_mvc_re_pr/controller/Cmain.js
+++ Node.js/14_mvc_re_pr/controller/Cmain.js
@@ -1,19 +1,5 @@
 const model = require('../model/Model');
 
 exports.main = (req,res) => {
-    res.render('index', {data: model});
-    
-};
-
-exports.gestplus = (req,res) => {
-    console.log(req.body)
-    models.data.create({
-        id:req.body.id,
-        name:req.body.name,
-        gender:req.body.gender,
-        major: req.body.major
-    }).then(result => {
-        console.log(result);
-        res.send({ id:req.body.id, name:req.body.name, gender:req.body.gender, major: req.body.major})
-    })
+    res.render('index', {data: model});  
 };
