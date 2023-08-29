fedae5f787c1fa5b01b3e7cd7014e6e2358a0f60
bliss1454 <rkgus1454@naver.com>
2023-08-29 23:42:50 +0900

    chat

--- Node.js/23_socket_io_chat/server.js
+++ Node.js/23_socket_io_chat/server.js
@@ -27,12 +27,19 @@ io.on('connection', (socket) => {
         socket.room = res; //방 id들을 담아준다. 채팅을 할 때, 어떤 방인지 알려줄 수 있음.
         console.log('조인 후', socket.rooms);
         //broadcast는 나를 제외한 전체사용자(브라우저)에게 메세지 전달
-        socket.broadcast.to(res).emit('create', '새로운 브라우저가 입장하였습니다')
+        socket.broadcast.to(res).emit('create', '새로운 브라우저가 입장하였습니다');
+        const roomInfo = io.sockets.adapter.rooms.get(res)?.size;
+        console.log(roomInfo);
     });
     socket.on('message', (res) => {
         //io.to(특정방id).emit(이벤트) : 특정방의 전체 사용자에게 메세지를 전달 한다.
         io.to(socket.room).emit('chat',res);
-    })
+    });
+    socket.on('leave', () => {
+        socket.leave(socket.room);
+        const roomInfo = io.sockets.adapter.rooms.get(socket.room)?.size;
+        console.log(roomInfo);
+    });
 });
 
 //서버
---
+++ Node.js/23_socket_io_chat2/server.js
@@ -0,0 +1,62 @@
+const http = require("http");
+const express = require("express");
+const SocketIO = require("socket.io");
+
+const app = express();
+const PORT = 8000;
+
+
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
+app.set("view engine", "ejs");
+
+// http 서버
+const server = http.createServer(app);
+// socket 서버
+const io = SocketIO(server);
+
+app.get('/', (req, res)=>{
+    res.render("chat");
+})
+
+io.on('connection', (socket)=>{
+    // console.log('조인 전', socket.rooms);
+    socket.on('join', (res)=>{
+        // console.log(`${res} 방 입장`);
+        // 채팅방을 생성하는 방법은 join(방아이디), 방이 존재하면 그 방으로 접속
+        socket.join(res);
+        socket.room = res;
+        
+        // broadcast 나를 제외한 전체사용자에게 메세지 전달
+        //socket.broadcast.to(res).emit('create', `${socket.id} 입장하였습니다.`);
+        //const roomInfo = io.sockets.adapter.rooms.get(res);
+        //console.log("접속인원:", roomInfo.size);
+    })
+
+    socket.on('message', (res)=>{
+        // 특정방의 전체사용자에게 메세지 전달
+        // console.log(socket.room);
+        socket.emit('chatMe', res);
+        socket.broadcast.to(socket.room).emit('chat', res);
+    })
+
+    socket.on('leave', ()=>{
+        socket.leave(socket.room);
+        const roomInfo = io.sockets.adapter.rooms.get(socket.room);
+        console.log("접속인원:", roomInfo.size);
+    } )
+
+    socket.on('open', (arg, cb)=>{
+        console.log(arg);
+        cb(arg);
+    });
+
+    socket.on('form_message', (arg)=>{
+        io.emit('back_message', arg)
+    })
+})
+
+
+server.listen(PORT, ()=>{
+    console.log(`http://localhost:${PORT}`);
+})
