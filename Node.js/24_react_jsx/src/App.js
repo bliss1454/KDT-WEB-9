9ef57d5b04ce372765d798290b0df3effb58a166
bliss1454 <rkgus1454@naver.com>
2023-09-18 20:51:30 +0900

    react

--- Node.js/24_react_jsx/src/App.js
+++ Node.js/24_react_jsx/src/App.js
@@ -1,31 +1,72 @@
 import logo from './logo.svg';
 import './App.css';
 
-// function App() {
+function App() {
+  const flag = false;
+  let txt = ''
+  if (flag) {
+    txt = 'true 입니다'
+  } else {
+    txt = 'false 입니다'
+  }
 
-//   const flag = true;
-//   let txt = ''
+  const styles = {
+    backgroundColor: 'red'
+  }
 
-//   if (flag) {
-//     txt = 'true 입니다'
-//   } else {
-//     txt = 'false 입니다'
-//   }
+  //Map 함수
+  const lists = ['k', 'd', 't', 'w', 'e' ,'b'];
+  //filter 함수
+  const animals = ['dog', 'cat', 'rabbit'];
+  const newAnimals = animals.filter( value => {
+    return value.includes('a')
+  })
+  console.log(newAnimals);
+  //단축평가-->&& ||
+  //&&
+  const result = true && "Hello";
+  console.log(result)
+  //||
+  const defaultValue = 1000
+  const price = 1500
+  const dbPrice = price || defaultValue;
+  console.log(dbPrice);
 
-//   const styles = {
-//     backgroundColor: 'red'
-//   }
+  //실습5
+  const users = [
+    { id: 1, name: "John", age: 25, vip: true },
+    { id: 2, name: "Jane", age: 19, vip: false },
+    { id: 3, name: "Alice", age: 30, vip: true },
+    { id: 4, name: "Bob", age: 18, vip: false },
+    { id: 5, name: "Charlie", age: 35, vip: true }
+  ];
+  const vipUsers = users. filter(value => value.vip === true) //filter로 구분 지어주고 이것을 vipUsers에 담아준다, 그리고 밑의 map함수를 통해서 필요한 값만 출력해준다.
+  console.log(vipUsers)
 
-//   return (
-//     <>
-//       <h1 style={ {backgroundColor: 'black', color: 'white'} } >Hello React</h1>
-//       <div style={styles}>리액트 시작</div>
-//       <input />
-//       <div>{flag ? <h1>true입니다</h1> : <h1>false입니다</h1>}</div>
-//       <div>{txt}</div>
-//     </>
-//   );
-// }
+  const isLogin = true; //isLogin의 값이 true이기에, 밑의 값들이 보일 수 있다. &&연산자는 앞의 값이 true이면 바로 해당 값을 꺼내준다. false이면 뒤의 값이 나온다.
+
+  return ( 
+    <>
+      <h1 style={ {backgroundColor: 'black', color: 'white'} } >Hello React</h1>
+      {isLogin && (
+        <>
+        <div style={styles}>리액트 시작</div>
+        <input />
+        <div>{flag ? <h1>true입니다</h1> : <h1>false입니다</h1>}</div>
+        <div>{txt}</div>
+          { lists.map((value, index) => {
+            return (
+              <div key={index}>
+                <p>Hello {value}</p>
+              </div>
+            )
+          })}
+        {vipUsers.map ((value) => <div key={value.id}>{value.name}</div>)}
+      </>
+      )}
+    </>
+  );
+}
 
 /*실습1*/ 
 // function App () {
@@ -75,18 +116,17 @@ import './App.css';
 // }
 
 /*실습4*/
- function App () {
-  return (
-    <>
-      <div className='red'></div>
-      <div className='orange'></div>
-      <div className='yellow'></div>
-      <div className='green'></div>
-      <div className='blue'></div>
-      <div className='navy'></div>
-      <div className='purple'></div>
-    </>
-  )
- }
+//  function App () {
+//   return (
+//     <>
+//       <div className='red'></div>
+//       <div className='orange'></div>
+//       <div className='yellow'></div>
+//       <div className='green'></div>
+//       <div className='blue'></div>
+//       <div className='navy'></div>
+//       <div className='purple'></div>
+//     </>
+//   );
 
 export default App;
