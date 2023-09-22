beeca2ceb7f9a9cf7d0be93f83868caa57e4a3d5
bliss1454 <rkgus1454@naver.com>
2023-09-22 14:49:15 +0900

    react

--- Node.js/react_basic/src/App.js
+++ Node.js/react_basic/src/App.js
@@ -1,41 +1,7 @@
-// import ClassComponent from './ClassComponent';
-// import FunctionComponent from './FunctionComponent';
-// import PracticeComponent from './PracticeComponent';
-// import FoodComponent from './FoodComponent';
-// import BookComponent from './BookComponent';
-// import Event from './Event'
-// import './App.css';
-// import EventClass from './EventClass';
-// import EventP from './EventP';
-//import Counter from './Counter';
-// import Counter from './Counter';
-// import Practice from './Practice';
-// import CounterFunc from './CounterFunc';
-// import ToggleFunc from './ToggleFunc'
-// import Practice2 from './Practice2'
-import LifeCycleClass from './LifeCycleClass'
+import Book from "./Book";
 
-// function App() {
-//   return (
-//     <>
-//       {/* <ClassComponent/>
-//       <FunctionComponent/>
-//       <ClassComponent/> */}
-//       {/* <ClassComponent name= "bliss" age={24}></ClassComponent>
-//       <ClassComponent /> */}
-//       {/* <FunctionComponent myClass={'kdt9'}>코딩</FunctionComponent>
-//       <FunctionComponent/> */}
-//       {/* <FoodComponent food={'국밥'} ></FoodComponent>
-//       <FoodComponent />
-//       <BookComponent title={'도시와 그 불확실한 벽'} author={'무라카미 하루키'} price={'19,500'} type={'소설'} ></BookComponent> */}
-//       {/* <Event />
-//       <EventClass /> */}
-//       <EventP message={"안녕하세요"} />
-        //  <Practice />
-        //  <ToggleFunc />
-        // <Practice2/>
-         <LifeCycleClass />
-//   );
-// };
+function App() {
+    return <></>;
+}
 
-export default LifeCycleClass;
+export default App;
