5fca769ef822c4e7a08cf69ff2237b09d395ec9a
bliss1454 <rkgus1454@naver.com>
2023-09-25 14:38:03 +0900

    react

---
+++ Node.js/react_study/src/App.js
@@ -0,0 +1,22 @@
+import logo from './logo.svg';
+import './App.css';
+import {useState} from "react"
+
+export const useInput = (initalValue, validator) => {
+  const [value, setValue] = useState(initalValue);
+  const onChange = event => {
+    const {
+      target: {value}
+    } = event;
+    let willUpdate = true;
+    if (typeof validator === "function") {
+      willUpdate = validator (value);
+    }
+    if (willUpdate) {
+      setValue(value);
+    }
+  };
+  return {value, onChange};
+}
+
+// export default App;
